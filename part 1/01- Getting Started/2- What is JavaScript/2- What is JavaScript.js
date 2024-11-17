eddcc8cf39e8eeff079a394c1e4efc44ee78c439
Reacher <macryze@icloud.com>
2024-11-17 20:05:36 +0800

    Add Prettier as a project dependency
    Added Prettier version 3.3.3 to the project's dependencies in `package.json` for code formatting consistency. Corresponding entries were also generated in the `yarn.lock` file to ensure reproducible builds.

---
+++ part 1/01- Getting Started/2- What is JavaScript/2- What is JavaScript.js
@@ -0,0 +1,4 @@
+// 1. Node.js was created by embedding Chrome's V8 JavaScript engine with C++. This combination enables JavaScript to run outside the browser.
+//    Node generally consists of the V8 engine (for JS execution), libuv (for handling async I/O), and various built-in modules for filesystem, HTTP, etc.
+
+// 2. JavaScript can run in two main environments: in a web browser (using the browser's JS engine) or in Node.js (a runtime that allows JS to run server-side).
