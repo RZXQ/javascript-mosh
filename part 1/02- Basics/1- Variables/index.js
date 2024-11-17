8afe95af34ac0a117ab4d0b728d3e63551ba7958
Reacher <macryze@icloud.com>
2024-11-18 00:47:05 +0800

    Add basic variables and naming conventions in index.js
    This commit introduces a new index.js file which demonstrates basic variable declaration and naming conventions in JavaScript. It covers the use of 'let', basic rules for valid variable names, and the importance of case sensitivity.

---
+++ part 1/02- Basics/1- Variables/index.js
@@ -0,0 +1,17 @@
+// 1. Variable: we use a variable to store data temporarily
+let name1; // from ES6+, we use let instead of var
+console.log(name1); // undefined
+
+let name2 = "Reacher";
+console.log(name2);
+
+// 2. Naming conventions:
+//  - cannot be a reserved keyword
+//  - should be meaningful
+//  - cannot start with a number(1name)
+//  - cannot container s space or hyphen(-)
+//  - are case-sensitive
+
+// we should declare each variable in a new line
+let firstName; // camel notation
+let FirstName; // those variable names are different
