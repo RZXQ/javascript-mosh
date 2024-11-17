e44129d3413647c12962724e33c293ad8aa38404
Reacher <macryze@icloud.com>
2024-11-18 00:50:29 +0800

    Add constants example for interest rates
    Created a new file in the Basics section to demonstrate the usage of constants in JavaScript. The example showcases declaring a constant for the interest rate and includes a commented-out line to illustrate that constants cannot be reassigned.

---
+++ part 1/02- Basics/2- Constants/index.js
@@ -0,0 +1,8 @@
+// 1. Declare a constant: if a variable doesn't need to be reassigned, it should be declared with 'const'.
+// Otherwise, it should be a variable declared with 'let'.
+const interestRate = 0.3;
+
+// The following line is commented out because it would cause an error: Constants cannot be reassigned.
+// interestRate = 1;
+
+console.log(interestRate);
