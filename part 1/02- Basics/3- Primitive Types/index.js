ac85ffea70cd9901216c72f2f83ff46b3d4605db
Reacher <macryze@icloud.com>
2024-11-19 00:15:30 +0800

    Add basic examples of primitive types in JavaScript
    Created a new file to demonstrate the use of primitive data types in JavaScript. The file includes examples of string, number, boolean, undefined, and null literals. This serves as an essential reference for understanding value types in JavaScript.

---
+++ part 1/02- Basics/3- Primitive Types/index.js
@@ -0,0 +1,10 @@
+// In JavaScript, there are two categories of types:
+// 1. Primitives (or Value Types)
+// 2. Reference Types
+
+// Primitives (or Value Types):
+let name = "Reacher"; // String literal
+let age = 30; // Number literal
+let isApproved = true; // Boolean literal
+let firstName = undefined; // Undefined
+let selectedColor = null; // Null
