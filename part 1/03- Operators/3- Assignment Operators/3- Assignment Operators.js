332b960e0b349fad8c7363eb361792fca1d5ce09
Reacher <macryze@icloud.com>
2024-11-19 18:06:04 +0800

    Add examples of assignment operators
    Created a new file to demonstrate basic assignment operators, including both standard and shorthand notation for addition and multiplication operations. This helps clarify the usage and effect of assignment operators in JavaScript.

---
+++ part 1/03- Operators/3- Assignment Operators/3- Assignment Operators.js
@@ -0,0 +1,7 @@
+let x = 10;
+
+x = x + 5; // Addition assignment
+x += 5; // Addition assignment (shorthand)
+
+x = x * 3; // Multiplication assignment
+x *= 3; // Multiplication assignment (shorthand)
