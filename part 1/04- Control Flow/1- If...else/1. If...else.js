30642ce077994c0feac82406b9169fd17aa425e9
Reacher <macryze@icloud.com>
2024-11-19 19:26:54 +0800

    Add initial time-based greeting logic
    This commit introduces a basic control flow for displaying greetings based on the time of day. Depending on the hour, it will print "Good morning," "Good afternoon," or "Good evening" to the console.

---
+++ part 1/04- Control Flow/1- If...else/1. If...else.js
@@ -0,0 +1,13 @@
+// Hour
+// if hour is between 6am and 12pm : Good morning!
+// if it is between 12pm and 6pm: Good afternoon!
+// otherwise: Good evening!
+
+let hour = 6;
+if (hour >= 6 && hour < 12) {
+  console.log("Good morning!");
+} else if (hour >= 12 && hour < 18) {
+  console.log("Good afternoon!");
+} else {
+  console.log("Good evening!");
+}
