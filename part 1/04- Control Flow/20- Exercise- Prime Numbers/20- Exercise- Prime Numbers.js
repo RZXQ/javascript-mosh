8fa18b771f2d80220d506ebbf602120e08697f6f
Reacher <macryze@icloud.com>
2024-11-20 01:48:33 +0800

    Add prime number exercise script
    Introduced a new JavaScript file to print prime numbers up to a given limit. The function iterates through numbers, checking for primality by testing divisibility. Primes are logged to the console if they pass the checks.

--- part 1/04- Control Flow/20- Exercise- Prime Numbers/20- Exercise- Prime Numbers.js
+++ part 1/04- Control Flow/20- Exercise- Prime Numbers/20- Exercise- Prime Numbers.js
@@ -1,17 +1,18 @@
 showPrimes(20);
 
 function showPrimes(limit) {
-  for (let i = 2; i <= limit; i++) {
-    let isPrime = true;
+  for (let number = 2; number <= limit; number++) {
+    if (isPrime(number)) {
+      console.log(number);
+    }
+  }
 
-    for (let j = 2; j < i; j++) {
-      if (i % j === 0) {
-        isPrime = false;
-        break;
+  function isPrime(number) {
+    for (let factor = 2; factor < number; factor++) {
+      if (number % factor === 0) {
+        return false;
       }
     }
-    if (isPrime) {
-      console.log(i);
-    }
+    return true;
   }
 }
