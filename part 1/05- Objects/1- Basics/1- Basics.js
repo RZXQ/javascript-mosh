2ee5659a8964813511d2a01699f84e3fe2ea6d5b
Reacher <macryze@icloud.com>
2024-11-30 00:18:14 +0800

    Refactor object method syntax
    Simplified the `draw` method syntax in the `circle` object for better readability and adherence to modern JavaScript practices. Added a comment to clarify terminology, specifying that a function within an object is called a method.

--- part 1/05- Objects/1- Basics/1- Basics.js
+++ part 1/05- Objects/1- Basics/1- Basics.js
@@ -1,9 +1,11 @@
 // Object-oriented Programming (OOP)
+
+// if a function is part of an object, then the function is called method
 const circle = {
   radius: 1,
   location: { x: 1, y: 1 },
   isVisible: true,
-  draw: function () {
+  draw() {
     console.log("draw");
   },
 };
