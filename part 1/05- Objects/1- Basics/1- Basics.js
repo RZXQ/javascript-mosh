74bc23987e735bbd298e05636f5a1206fadfe73b
Reacher <macryze@icloud.com>
2024-11-30 00:16:48 +0800

    Refactor circle object and simplify comments
    Simplify the example by removing excessive comments and switching from `let` to `const` for the circle object definition. The concise comments still convey the essentials of object-oriented programming in JavaScript.

--- part 1/05- Objects/1- Basics/1- Basics.js
+++ part 1/05- Objects/1- Basics/1- Basics.js
@@ -1,13 +1,11 @@
-/// This is an example of Object-oriented Programming (OOP) in JavaScript.
-/// It illustrates an object with properties and a method.
-let circle = {
-  radius: 1, // 1. Circle radius
-  location: { x: 1, y: 1 }, // 2. Circle's center location
-  isVisible: true, // 3. Visibility status
+// Object-oriented Programming (OOP)
+const circle = {
+  radius: 1,
+  location: { x: 1, y: 1 },
+  isVisible: true,
   draw: function () {
-    // 4. Method to draw the circle
     console.log("draw");
   },
 };
 
-circle.draw(); // Invokes the draw method
+circle.draw();
