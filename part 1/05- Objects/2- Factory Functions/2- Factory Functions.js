5d28c4a87286204a9d23838cb953106d4c9524af
Reacher <macryze@icloud.com>
2024-11-30 00:34:30 +0800

    Simplify comments in factory function example.
    Removed unnecessary comments to enhance code readability and focus. Comments explaining obvious actions were deleted, while retaining the succinct description of the factory function purpose.

--- part 1/05- Objects/2- Factory Functions/2- Factory Functions.js
+++ part 1/05- Objects/2- Factory Functions/2- Factory Functions.js
@@ -1,15 +1,15 @@
 function createCircle(radius) {
   return {
-    radius, // 1. Circle radius
+    radius,
     draw() {
-      // 2. Method to draw the circle
       console.log("draw");
     },
   };
 }
 
-const circle1 = createCircle(1); // Create circle1 with radius 1
+// Factory Function: produce objects
+const circle1 = createCircle(1);
 console.log(circle1);
 
-const circle2 = createCircle(2); // Create circle2 with radius 2
+const circle2 = createCircle(2);
 console.log(circle2);
