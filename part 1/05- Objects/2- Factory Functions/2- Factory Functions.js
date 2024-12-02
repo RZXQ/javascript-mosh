c8b8059582cd135392a0479a10fc5a2effab21ba
Reacher <macryze@icloud.com>
2024-12-03 00:12:18 +0800

    Refactor factory function for object creation
    Centralize the logic for creating objects using a factory function, providing clarity on its use with both shorthand and traditional function declarations. This enhances the maintainability and readability of the code by adopting standardized object creation patterns.

--- part 1/05- Objects/2- Factory Functions/2- Factory Functions.js
+++ part 1/05- Objects/2- Factory Functions/2- Factory Functions.js
@@ -1,14 +1,21 @@
-// Factory Function
+// Using a factory function to create an object.
+// It centralizes the logic for object creation.
 function createCircle(radius) {
   return {
-    radius,
+    radius, // Shorthand property name when key and value are the same.
+
+    // A. Declaring a method using shorthand syntax
     draw() {
       console.log("draw");
     },
+
+    // B. Traditional function declaration syntax
+    // draw: function () {
+    //   console.log("draw");
+    // }
   };
 }
 
-// Factory Function: produce objects
 const circle1 = createCircle(1);
 console.log(circle1);
 
