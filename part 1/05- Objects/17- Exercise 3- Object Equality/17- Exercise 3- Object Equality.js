0cab92021d367731f98ccd066c6ab8f2c0ebdb1c
Reacher <macryze@icloud.com>
2024-12-04 13:47:40 +0800

    Add test for object identity in Exercise 3
    Introduce a console log to test if address1 and address2 refer to the same object using the areSame function. This helps verify both object equality and identity aspects in the exercise.

--- part 1/05- Objects/17- Exercise 3- Object Equality/17- Exercise 3- Object Equality.js
+++ part 1/05- Objects/17- Exercise 3- Object Equality/17- Exercise 3- Object Equality.js
@@ -19,3 +19,4 @@ function areSame(address1, address2) {
 let address1 = new Address("a", "b", "c");
 let address2 = new Address("a", "b", "c");
 console.log(areEqual(address1, address2));
+console.log(areSame(address1, address2));
