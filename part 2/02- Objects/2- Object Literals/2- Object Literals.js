6364c20e5368f51eeb733aba0f29907343311f88
Reacher <macryze@icloud.com>
2024-12-12 19:54:55 +0800

    Add example demonstrating object literals in JavaScript
    Introduced a new file showcasing the use of object literals. The example includes properties, nested objects, and a method to illustrate how objects are structured and used in JavaScript.

---
+++ part 2/02- Objects/2- Object Literals/2- Object Literals.js
@@ -0,0 +1,15 @@
+const circle = {
+  // properties
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  draw: function () {
+    // method
+    console.log("draw");
+  },
+};
+
+console.log(circle.radius);
+circle.draw();
