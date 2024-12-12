caa8a62280926ba999ebf3ac3d56437740f5531c
Reacher <macryze@icloud.com>
2024-12-12 16:46:19 +0800

    Refactor: Remove dynamic function creation with Function constructor
    This change eliminates the use of the Function constructor for creating a dynamic function, simplifying the code and improving readability. The updated version focuses on more conventional patterns for function and object handling.

--- part 1/05- Objects/6- Functions are Objects/6- Functions are Objects.js
+++ part 1/05- Objects/6- Functions are Objects/6- Functions are Objects.js
@@ -10,23 +10,14 @@ function Circle(radius) {
   };
 }
 
+// 2. Creating an instance using the `new` keyword
 const circle = new Circle(1); // Creates a new Circle instance
+console.log(circle);
 
-// 2. Function properties (functions are objects)
+// 3. Function properties (functions are objects)
 console.log(Circle.name); // Function name: "Circle"
 console.log(Circle.length); // Number of parameters: 1
 console.log(Circle.constructor); // Constructor reference: [Function: Function]
-
-// 3. Creating a function dynamically using Function constructor
-const Circle1 = new Function(
-  "radius",
-  `  this.radius = radius;
-  this.draw = function () {
-    console.log("draw");
-  };`,
-);
-const circle2 = new Circle1(1); // Dynamically created Circle instance
-console.log(circle2);
 
 // 4. Using the `call` method to invoke the Circle constructor
 Circle.call({}, 1); // Calls Circle with `this` bound to an empty object
