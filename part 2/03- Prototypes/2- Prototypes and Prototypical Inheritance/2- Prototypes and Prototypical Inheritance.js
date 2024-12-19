a01ca3d2d127fb0d1cef0fbafe6439c6acc0e537
Reacher <macryze@icloud.com>
2024-12-20 02:03:48 +0800

    Add explanation about root object in prototypes example
    Expanded the comment to clarify that every object has a prototype, except the root object (Object.prototype). This enhances the educational value of the code by providing more context about prototypes.

--- part 2/03- Prototypes/2- Prototypes and Prototypical Inheritance/2- Prototypes and Prototypical Inheritance.js
+++ part 2/03- Prototypes/2- Prototypes and Prototypical Inheritance/2- Prototypes and Prototypical Inheritance.js
@@ -1,5 +1,6 @@
 let x = {};
 let y = {};
 
-// A prototype is just a regular object
-console.log(Object.getPrototypeOf(x) === Object.getPrototypeOf(y)) // true
+// A prototype is just a regular object.
+// Every object has a prototype, except the root object (Object.prototype).
+console.log(Object.getPrototypeOf(x) === Object.getPrototypeOf(y)); // true
