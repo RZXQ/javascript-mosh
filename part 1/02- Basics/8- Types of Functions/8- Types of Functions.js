70f5b8e5b4a37a2cf3f5de1ae0fadfbf30706beb
Reacher <macryze@icloud.com>
2024-12-27 16:34:58 +0800

    Refactor comments for clarity and remove redundancy.
    Simplified comments by removing unnecessary details and redundant outputs. This improves code readability and focuses on essential information in the documentation.

--- part 1/02- Basics/8- Types of Functions/8- Types of Functions.js
+++ part 1/02- Basics/8- Types of Functions/8- Types of Functions.js
@@ -5,16 +5,15 @@
 // : is called a colon
 
 // 1. Function performing a task
-// This function accepts two parameters (firstName and lastName) and prints a greeting message
 function greet(firstName, lastName) {
   console.log("Hello " + firstName + " " + lastName);
 }
 
-greet("Reacher", "WANG"); // Output: "Hello Reacher WANG"
+greet("Reacher", "WANG");
 
 // 2. Function calculating a value
 function square(number) {
-  return number * number; // The function returns the result, so we can use it as a value
+  return number * number;
 }
 
 console.log(square(2));
