5cad52203db05f79874a48548fbd025d52027961
Reacher <macryze@icloud.com>
2024-12-27 19:28:06 +0800

    Simplify and clarify for...in loop examples.
    Removed excess comments to streamline the code and focus on core examples. This improves readability and highlights the primary usage of the for...in loop for both objects and arrays.

--- part 1/04- Control Flow/7- For...in/7- For...in.js
+++ part 1/04- Control Flow/7- For...in/7- For...in.js
@@ -1,21 +1,9 @@
-// Traditional loops: for, while, do-while
+const person = { name: "Reacher", age: 33 };
 
-// Extra JavaScript loop: for...in loop
-// Use case: iterate over the properties of an object or elements in an array
-
-// A. for...in loop example with an object
-const person = {
-  name: "Reacher",
-  age: 33,
-};
-
-// Loop over the properties of the object
 for (let key in person) {
-  // Dot notation is not applicable here; using bracket notation to access the value
   console.log(key, person[key]);
 }
 
-// for...in loop example with an array
 const colors = ["red", "green", "blue"];
 for (let index in colors) {
   console.log(index, colors[index]);
