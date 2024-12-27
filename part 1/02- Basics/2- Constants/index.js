79d5e4339f9a04d9292440b41b44f73e8cc586c3
Reacher <macryze@icloud.com>
2024-12-27 15:04:48 +0800

    Simplify constant declaration comments.
    Refactored comments to be concise and directly relevant. Removed redundant lines explaining basic constant behavior for better clarity and maintainability.

--- part 1/02- Basics/2- Constants/index.js
+++ part 1/02- Basics/2- Constants/index.js
@@ -1,7 +1,2 @@
-// 1. Declare a constant: if a variable doesn't need to be reassigned, it should be declared with 'const'.
-// Otherwise, it should be a variable declared with 'let'.
-const interestRate = 0.3;
-
-// The following line is commented out because it would cause an error: Constants cannot be reassigned.
-// interestRate = 1;
+const interestRate = 0.3; // Constants cannot be reassigned
 console.log(interestRate);
