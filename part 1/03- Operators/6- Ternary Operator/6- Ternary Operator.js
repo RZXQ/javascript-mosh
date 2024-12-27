8b751cd58766c9b5c382b59f73dad738dd5fdfa3
Reacher <macryze@icloud.com>
2024-12-27 18:28:49 +0800

    Simplify comments in ternary operator example.
    Removed overly detailed comments to make the code cleaner and more concise. The updated comment provides a straightforward description without redundancy.

--- part 1/03- Operators/6- Ternary Operator/6- Ternary Operator.js
+++ part 1/03- Operators/6- Ternary Operator/6- Ternary Operator.js
@@ -1,6 +1,4 @@
-// If a customer has more than 100 points, they are a 'gold' customer
-// Otherwise, they are a 'silver' customer
-
+// Ternary operator
 let points = 110;
-let type = points > 100 ? "gold" : "silver"; // Ternary operator for determining customer type
+let type = points > 100 ? "gold" : "silver";
 console.log(type);
