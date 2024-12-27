af8f872d3d808982ec930a38364fb89e5e04ca2c
Reacher <macryze@icloud.com>
2024-12-27 16:37:57 +0800

    Refactor comments for function categories clarity
    Simplified and rephrased comments to make the function categories more concise and direct. This improves readability and aligns the descriptions with the actual function purpose. No functional changes were made.

--- part 1/02- Basics/8- Types of Functions/8- Types of Functions.js
+++ part 1/02- Basics/8- Types of Functions/8- Types of Functions.js
@@ -4,14 +4,14 @@
 // , is called a comma
 // : is called a colon
 
-// 1. Function performing a task
+// 1. Performing a task
 function greet(firstName, lastName) {
   console.log("Hello " + firstName + " " + lastName);
 }
 
 greet("Reacher", "WANG");
 
-// 2. Function calculating a value
+// 2. Calculating a value
 function square(number) {
   return number * number;
 }
