6dc932e096324623a761904f55250abd4b645726
Reacher <macryze@icloud.com>
2024-12-28 15:10:25 +0800

    Simplify function object examples in code.
    Removed verbose comments and redundant examples to streamline the code. Retained key demonstrations of `name`, `length`, `constructor`, `call`, and `apply` for clarity.

--- part 1/05- Objects/6- Functions are Objects/6- Functions are Objects.js
+++ part 1/05- Objects/6- Functions are Objects/6- Functions are Objects.js
@@ -1,8 +1,3 @@
-/// 1. This example demonstrates that functions in JavaScript are objects.
-///    - Functions have properties like `name`, `length`, and `constructor`.
-///    - Functions can be used as constructors to create objects using the `new` keyword.
-///    - Functions provide utility methods like `call` and `apply`.
-
 function Circle(radius) {
   this.radius = radius;
   this.draw = function () {
@@ -10,20 +5,10 @@ function Circle(radius) {
   };
 }
 
-// 2. Creating an instance using the `new` keyword
-const circle = new Circle(1); // Creates a new Circle instance
-console.log(circle);
+console.log(Circle.name); // "Circle"
+console.log(Circle.length); // 1
+console.log(Circle.constructor); // [Function: Function]
 
-// 3. Function properties (functions are objects)
-console.log(Circle.name); // Function name: "Circle"
-console.log(Circle.length); // Number of parameters: 1
-console.log(Circle.constructor); // Constructor reference: [Function: Function]
-
-// 4. Using the `call` method to invoke the Circle constructor
-Circle.call({}, 1); // Calls Circle with `this` bound to an empty object
-
-// 5. Explanation: `new Circle(1)` is equivalent to Circle.call({}, 1).
-const another = new Circle(1); // Internally similar to the above call
-
-// 6. Using the `apply` method to invoke the Circle constructor
-Circle.apply({}, [1]); // Similar to `call` but takes arguments as an array
+const obj = {};
+Circle.call(obj, 1); // Sets obj.radius = 1
+Circle.apply(obj, [1]); // Same as call, with arguments as an array
