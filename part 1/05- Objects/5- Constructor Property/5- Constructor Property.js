eb52686547889726a92d193c931df8476616c458
Reacher <macryze@icloud.com>
2024-12-28 15:02:03 +0800

    Refactor constructor property examples and add HTML file
    Revised JavaScript examples to demonstrate constructor properties using both factory and constructor functions. Removed verbose comments for clarity. Added an HTML file to facilitate script testing in a browser.

--- part 1/05- Objects/5- Constructor Property/5- Constructor Property.js
+++ part 1/05- Objects/5- Constructor Property/5- Constructor Property.js
@@ -1,5 +1,3 @@
-// 1. Every object in JavaScript has a property called 'constructor'.
-//    - The 'constructor' property references the function that was used to create the object.
 function Circle(radius) {
   this.radius = radius;
   this.draw = function () {
@@ -7,8 +5,20 @@ function Circle(radius) {
   };
 }
 
-const circle = new Circle(1);
-console.log(circle.constructor); // Output: [Function: Circle]
+function createCircle(radius) {
+  return {
+    radius,
+    draw() {
+      console.log("draw");
+    },
+  };
+}
+
+const circle1 = new Circle(1);
+console.log(circle1.constructor); // Output: [Function: Circle] (Circle was created using a constructor function)
 
 let x = {};
-console.log(x.constructor); // Output: [Function: Object]
+console.log(x.constructor); // Output: [Function: Object] (x was created using the Object constructor by default)
+
+const circle2 = createCircle(1);
+console.log(circle2.constructor); // Output: [Function: Object] (circle2 is created using a factory function, which creates plain objects)
