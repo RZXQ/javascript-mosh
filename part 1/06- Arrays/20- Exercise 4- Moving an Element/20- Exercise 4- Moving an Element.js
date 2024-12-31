76a49ec3ae21fe157ca6f40cbb809da2a4ed1e25
Reacher <macryze@icloud.com>
2025-01-01 01:43:11 +0800

    Refactor "move" function with enhanced readability.
    Added section headers and clarified comments to improve code organization and readability. These changes explicitly highlight validation, functionality, and purpose for easier maintenance and understanding. No functional behavior is altered.

--- part 1/06- Arrays/20- Exercise 4- Moving an Element/20- Exercise 4- Moving an Element.js
+++ part 1/06- Arrays/20- Exercise 4- Moving an Element/20- Exercise 4- Moving an Element.js
@@ -1,20 +1,22 @@
 const numbers = [1, 2, 3, 4];
 
+// ==================== Move Element ====================
 const output = move(numbers, 1, 2);
-
 console.log(output);
 
+// ==================== Function: move ====================
 function move(array, index, offset) {
   const position = index + offset;
 
-  // validation
+  // --- Validation ---
   if (position >= array.length || position < 0) {
     console.error("Invalid offset.");
+    return;
   }
 
   const output = [...array];
 
-  // cut and paste elements
+  // --- Cut and Paste ---
   const element = output.splice(index, 1)[0];
   output.splice(position, 0, element);
 
