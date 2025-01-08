79c92d956977b0d1315217ab4f3f06a0366859b9
Reacher <macryze@icloud.com>
2025-01-09 01:43:14 +0800

    Clarify constructor property usage with improved comments.
    Added a detailed explanation above the code to clarify the purpose and behavior of the constructor property. This improves code readability and helps developers understand the underlying concept better.

--- part 2/02- Objects/5- Constructor Property/5- Constructor Property.js
+++ part 2/02- Objects/5- Constructor Property/5- Constructor Property.js
@@ -1,2 +1,5 @@
-let x = {}; // equals to let x = new Object();
-console.log(x.constructor);
+// -----------------------------------------------------------
+// Every object has a constructor property referencing its creator function
+// -----------------------------------------------------------
+let x = {}; // Equivalent to: let x = new Object()
+console.log(x.constructor); // Logs: [Function: Object]
