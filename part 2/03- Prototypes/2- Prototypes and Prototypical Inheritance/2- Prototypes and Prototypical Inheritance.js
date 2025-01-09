965720d2049425d4905a38e7b0397770b412f1c4
Reacher <macryze@icloud.com>
2025-01-09 17:47:58 +0800

    Add HTML file and update comments in JS for prototypes
    Introduced a new HTML file to link associated JS content and updated the comments for clarity in the JavaScript file. The changes improve structure and provide a clearer explanation of prototypes and inheritance basics.

--- part 2/03- Prototypes/2- Prototypes and Prototypical Inheritance/2- Prototypes and Prototypical Inheritance.js
+++ part 2/03- Prototypes/2- Prototypes and Prototypical Inheritance/2- Prototypes and Prototypical Inheritance.js
@@ -1,6 +1,6 @@
+// A prototype is just a regular object.
+// Every object has a prototype, except the ObjectBase
+
 let x = {};
 let y = {};
-
-// A prototype is just a regular object.
-// Every object has a prototype, except the root object (Object.prototype).
 console.log(Object.getPrototypeOf(x) === Object.getPrototypeOf(y)); // true
