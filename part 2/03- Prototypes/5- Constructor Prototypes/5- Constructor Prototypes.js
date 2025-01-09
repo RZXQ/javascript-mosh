d1576786199c2cffa9c5e983531a2848b205ab97
Reacher <macryze@icloud.com>
2025-01-09 20:07:26 +0800

    Add comments to clarify prototype and constructor links
    Enhanced the documentation with additional comments explaining the relationships between prototypes, constructors, and their instances. This aims to improve understanding of the prototype chain and the `constructor` property.

--- part 2/03- Prototypes/5- Constructor Prototypes/5- Constructor Prototypes.js
+++ part 2/03- Prototypes/5- Constructor Prototypes/5- Constructor Prototypes.js
@@ -1,10 +1,12 @@
-//             Constructor Function (e.g., Circle)
+// ===========================================================
+//                  Constructor Function (e.g., Circle)
+// ===========================================================
 //             +---------------------------+
 //             |       Circle(radius)      |
 //             +---------------------------+
-//                        |
-//                        | .prototype
-//                        v
+//               |               ^
+//               | .prototype    | .constructor
+//               v               |
 //             +---------------------------+
 // Parent       |     Circle.prototype     |
 // Prototype    | (Shared by all instances)|
@@ -17,6 +19,7 @@
 // Child        |        circle            |
 // Instance     | Instance of Circle       |
 //             +---------------------------+
+
 // ===========================================================
 //                      Circle Prototype
 // ===========================================================
@@ -25,9 +28,11 @@ function Circle(radius) {
 }
 
 const circle = new Circle(1);
+
 console.log(circle.__proto__); // Points to Circle.prototype
 console.log(Object.getPrototypeOf(circle)); // Same as circle.__proto__
 console.log(Circle.prototype); // Prototype object of the Circle constructor
+console.log(Circle.prototype.constructor); // Points back to Circle
 
 // ===========================================================
 //                      Array Prototype
@@ -36,3 +41,4 @@ let array = [];
 console.log(array.__proto__); // Points to Array.prototype
 console.log(Object.getPrototypeOf(array)); // Same as array.__proto__
 console.log(Array.prototype); // Prototype object of the Array constructor
+console.log(Array.prototype.constructor); // Points back to Array
