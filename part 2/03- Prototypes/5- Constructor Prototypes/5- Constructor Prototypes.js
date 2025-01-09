58d4d8267261f414ef0742e401785020855bbb9a
Reacher <macryze@icloud.com>
2025-01-09 19:54:50 +0800

    Add Constructor Prototypes examples in JS and HTML files
    Introduced a JavaScript file demonstrating Circle and Array prototype concepts, including their relationships with constructors and instances. Added a corresponding HTML file to link and execute the script. This aids in understanding prototype inheritance and constructor behavior.

---
+++ part 2/03- Prototypes/5- Constructor Prototypes/5- Constructor Prototypes.js
@@ -0,0 +1,19 @@
+// ===========================================================
+//                      Circle Prototype
+// ===========================================================
+function Circle(radius) {
+  this.radius = radius;
+}
+
+const circle = new Circle(1);
+console.log(circle.__proto__); // Points to Circle.prototype
+console.log(Object.getPrototypeOf(circle)); // Same as circle.__proto__
+console.log(Circle.prototype); // Prototype object of the Circle constructor
+
+// ===========================================================
+//                      Array Prototype
+// ===========================================================
+let array = [];
+console.log(array.__proto__); // Points to Array.prototype
+console.log(Object.getPrototypeOf(array)); // Same as array.__proto__
+console.log(Array.prototype); // Prototype object of the Array constructor
