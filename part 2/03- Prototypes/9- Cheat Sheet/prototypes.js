e98026a865462a211c24e70c3463f579bc097510
Reacher <macryze@icloud.com>
2025-01-11 00:41:48 +0800

    Simplify and reorganize prototype-related examples.
    Streamlined the structure and content of prototype examples for better clarity and efficiency. Removed redundant sections, consolidated key concepts, and emphasized practical use cases for prototypes and property attributes.

--- part 2/03- Prototypes/9- Cheat Sheet/prototypes.js
+++ part 2/03- Prototypes/9- Cheat Sheet/prototypes.js
@@ -1,92 +1,60 @@
-//--------------------------------------------------
-// Prototypes and Prototypical Inheritance
-//--------------------------------------------------
+// --------------------------------------------------
+//                 Prototype Overview
+// --------------------------------------------------
 // Every object (except the root object) has a prototype (parent).
-// Use Object.getPrototypeOf(obj) to get the prototype of an object.
-let x = {},
-  y = {};
-console.log(Object.getPrototypeOf(x) === Object.getPrototypeOf(y)); // true
+// To get the prototype of an object:
+Object.getPrototypeOf(obj);
 
-//--------------------------------------------------
-// Multilevel Inheritance
-//--------------------------------------------------
-// Example of prototype chains in arrays and custom constructors
-let arr = [];
-console.log(arr); // Array with its prototype (Array.prototype)
+// In Chrome, you can inspect "__proto__" property. But you should
+// not use that in the code.
 
-function Circle(radius) {
-  this.radius = radius;
-  this.draw = () => console.log("draw");
-}
-const circle = new Circle(10);
-console.log(circle); // Circle instance and its prototype (Circle.prototype)
+// --------------------------------------------------
+//           Property Attributes and Descriptors
+// --------------------------------------------------
+// To get the attributes of a property:
+Object.getOwnPropertyDescriptor(obj, "propertyName");
 
-//--------------------------------------------------
-// Property Descriptors
-//--------------------------------------------------
-// Use Object.getOwnPropertyDescriptor to inspect attributes
-let person = { name: "Reacher" };
-console.log(
-  Object.getOwnPropertyDescriptor(Object.getPrototypeOf(person), "toString"),
-);
-
-// Use Object.defineProperty to set attributes
-Object.defineProperty(person, "name", {
+// To set the attributes for a property:
+Object.defineProperty(obj, "propertyName", {
+  configurable: false, // cannot be deleted
   writable: false,
   enumerable: false,
-  configurable: false,
 });
-person.name = "Jack"; // No effect
-console.log(person.name); // "Reacher"
-console.log(Object.keys(person)); // []
 
-//--------------------------------------------------
-// Constructor Prototypes
-//--------------------------------------------------
-// Prototypes are shared among all objects created by the constructor.
-function CirclePrototype(radius) {
-  this.radius = radius;
+// --------------------------------------------------
+//          Constructor and Prototype Property
+// --------------------------------------------------
+// Constructors have a "prototype" property. It returns the object
+// that will be used as the prototype for objects created by the constructor.
+Object.prototype === Object.getPrototypeOf({});
+Array.prototype === Object.getPrototypeOf([]);
+
+// --------------------------------------------------
+//      Shared Prototype Across Constructor Instances
+// --------------------------------------------------
+// All objects created with the same constructor will have the same prototype.
+// A single instance of this prototype will be stored in the memory.
+const x = {};
+const y = {};
+Object.getPrototypeOf(x) === Object.getPrototypeOf(y); // returns true
+
+// Any changes to the prototype will be immediately visible to all objects
+// referencing this prototype.
+
+// --------------------------------------------------
+//      Efficient Use of Prototypes for Methods
+// --------------------------------------------------
+// When dealing with large number of objects, it's better to put their
+// methods on their prototype. This way, a single instance of the methods
+// will be in the memory.
+Circle.prototype.draw = function () {};
+
+// --------------------------------------------------
+//          Accessing Object Properties
+// --------------------------------------------------
+// To get the own/instance properties:
+Object.keys(obj);
+
+// To get all the properties (own + prototype):
+for (let key in obj) {
 }
-CirclePrototype.prototype.draw = () => console.log("draw");
-
-const protoCircle = new CirclePrototype(1);
-console.log(Object.getPrototypeOf(protoCircle) === CirclePrototype.prototype); // true
-console.log(CirclePrototype.prototype.constructor === CirclePrototype); // true
-
-//--------------------------------------------------
-// Prototype vs Instance Members
-//--------------------------------------------------
-// Prototype members are shared; instance members are unique.
-function CircleInstance(radius) {
-  this.radius = radius; // Instance member
-}
-CircleInstance.prototype.draw = function () {
-  console.log("draw"); // Prototype member
-};
-const c1 = new CircleInstance(1);
-console.log(c1.toString()); // Default toString or custom implementation
-
-//--------------------------------------------------
-// Iterating Instance and Prototype Members
-//--------------------------------------------------
-// Object.keys() returns instance properties.
-// for...in includes both instance and prototype properties.
-function CircleIteration(radius) {
-  this.radius = radius;
-  this.move = () => console.log("move");
-}
-CircleIteration.prototype.draw = () => console.log("draw");
-
-const c2 = new CircleIteration(1);
-console.log(Object.keys(c2)); // ["radius", "move"]
-for (let key in c2) console.log(key); // Includes "draw" from prototype
-
-//--------------------------------------------------
-// Avoid Extending Built-in Objects
-//--------------------------------------------------
-// Avoid modifying objects you don't own.
-Array.prototype.shuffle = function () {
-  // Custom shuffle logic
-};
-const myArray = [];
-myArray.shuffle();
