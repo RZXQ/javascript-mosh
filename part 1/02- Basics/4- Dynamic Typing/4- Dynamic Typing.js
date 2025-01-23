66dff4a2a67c216b15a78e21cc2236b14abf6ad7
Reacher <macryze@icloud.com>
2025-01-24 01:49:13 +0800

    Add section headers and Symbol type example
    Introduced clear section headers for better readability and organization in the dynamic typing examples. Also added an example to demonstrate the Symbol type to enrich content and improve completeness.

--- part 1/02- Basics/4- Dynamic Typing/4- Dynamic Typing.js
+++ part 1/02- Basics/4- Dynamic Typing/4- Dynamic Typing.js
@@ -1,29 +1,53 @@
-// Typing in Programming:
+// ===========================================================
+//                        Typing in Programming
+// ===========================================================
 // 1. Static: Variable types are fixed (e.g., `String name = "John";`).
 // 2. Dynamic: Variable types are determined at runtime (e.g., `let name = "John";`).
 
-// Dynamic Typing in JavaScript
+// ===========================================================
+//                  Dynamic Typing in JavaScript
+// ===========================================================
+
 let name = "Reacher"; // Initially a string
 console.log(typeof name); // Output: "string"
 
 name = 1; // Now a number
 console.log(typeof name); // Output: "number"
 
-// Number Type
+// ===========================================================
+//                      Number Type
+// ===========================================================
+
 let age = 30;
 console.log(typeof age); // Output: "number"
 
 age = 30.1; // Still a "number" (no distinction between integers & floats)
 console.log(typeof age); // Output: "number"
 
-// Boolean Type
+// ===========================================================
+//                      Boolean Type
+// ===========================================================
+
 let isApproved = true;
 console.log(typeof isApproved); // Output: "boolean"
 
-// Undefined Type
+// ===========================================================
+//                      Undefined Type
+// ===========================================================
+
 let firstName;
 console.log(typeof firstName); // Output: "undefined"
 
-// Null Type
+// ===========================================================
+//                        Null Type
+// ===========================================================
+
 let selectedColor = null;
 console.log(typeof selectedColor); // Output: "object" (a known JavaScript quirk)
+
+// ===========================================================
+//                       Symbol Type
+// ===========================================================
+
+let symbol = Symbol();
+console.log(typeof Symbol()); // Output: "symbol"
