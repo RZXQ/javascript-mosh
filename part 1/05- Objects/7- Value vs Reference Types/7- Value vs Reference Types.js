891115366017b135fd0e72e66b2a7bd7febdad89
Reacher <macryze@icloud.com>
2025-01-26 15:53:14 +0800

    Refactor comments for improved readability in examples
    Updated section headers to be more descriptive and consistent with formatting. Added illustrative comments to clarify the expected outputs of examples for better understanding.

--- part 1/05- Objects/7- Value vs Reference Types/7- Value vs Reference Types.js
+++ part 1/05- Objects/7- Value vs Reference Types/7- Value vs Reference Types.js
@@ -1,35 +1,35 @@
-// ==================== Primitive Types ====================
-
+// ===========================================================
+//                     Primitive Types
+// ===========================================================
 let x1 = 10;
 let y1 = x1;
 x1 = 20;
-console.log(x1, y1);
+console.log(x1, y1); // 20, 10
 
-// ================= Reference Types =================
-
+// ===========================================================
+//                    Reference Types
+// ===========================================================
 let x2 = { value: 10 };
 let y2 = x2;
 x2.value = 20;
-console.log(x2, y2);
+console.log(x2, y2); // { value: 20 }, { value: 20 }
 
-// ================= Primitive Types in Function =================
-
+// ===========================================================
+//            Primitive Types in Function
+// ===========================================================
 let number = 10;
-
 function increase1(number) {
   number++;
 }
-
 increase1(number);
-console.log(number);
+console.log(number); // 10
 
-// ================= Reference Types in Function =================
-
+// ===========================================================
+//            Reference Types in Function
+// ===========================================================
 let obj = { value: 10 };
-
 function increase2(obj) {
   obj.value++;
 }
-
 increase2(obj);
-console.log(obj);
+console.log(obj); // { value: 11 }
