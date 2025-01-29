6a43c718dd06504b5c18c698b2d4a511b75451d4
Reacher <macryze@icloud.com>
2025-01-29 23:37:04 +0800

    Refactor and reformat array reduction examples
    Simplified variable names in `reduce()` callbacks for clarity. Improved section headers for better readability and consistency.

--- part 1/06- Arrays/16- Reducing an Array/16- Reducing an Array.js
+++ part 1/06- Arrays/16- Reducing an Array/16- Reducing an Array.js
@@ -1,14 +1,15 @@
 const numbers = [1, -1, 2, 3];
 
-// ==================== 1. Using `reduce()` with an Initial Value ====================
-const sum1 = numbers.reduce(
-  (accumulator, currentValue) => accumulator + currentValue,
-  0,
-);
+// ===========================================================
+//                  1. `reduce()` with Initial Value
+// ===========================================================
+
+const sum1 = numbers.reduce((acc, curr) => acc + curr, 0);
 console.log(sum1);
 
-// ==================== 2. Using `reduce()` without an Initial Value ====================
-const sum2 = numbers.reduce(
-  (accumulator, currentValue) => accumulator + currentValue,
-);
+// ===========================================================
+//               2. `reduce()` without Initial Value
+// ===========================================================
+
+const sum2 = numbers.reduce((acc, curr) => acc + curr);
 console.log(sum2);
