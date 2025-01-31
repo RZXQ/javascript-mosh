655a35bb4722e410f8f56659dc470204feee1d02
Reacher <macryze@icloud.com>
2025-02-01 01:54:44 +0800

    Add section header for Object Getters & Setters
    Introduced a visual section header to improve code organization and readability. This makes it easier to identify and navigate the code related to getters and setters. No functional changes were made.

--- part 1/07- Functions/6- Getters and Setters/6- Getters and Setters.js
+++ part 1/07- Functions/6- Getters and Setters/6- Getters and Setters.js
@@ -1,13 +1,17 @@
+// ===========================================================
+//                      Object Getters & Setters
+// ===========================================================
+
 const person = {
   firstName: "Mosh",
   lastName: "Hamedani",
 
-  // 1. Getter
+  // Getter
   get fullName() {
     return `${this.firstName} ${this.lastName}`;
   },
 
-  // 2. Setter
+  // Setter
   set fullName(value) {
     this.firstName = value.split(" ")[0];
     this.lastName = value.split(" ")[1];
