fbddf359dd41e4f054f93c3f2c2afd69ae887716
Reacher <macryze@icloud.com>
2025-02-06 14:48:29 +0800

    Add comments explaining property addition/deletion methods
    Explain the use of dot and bracket notation for adding properties, including when to use each. Improve readability by structuring the code with section comments.

--- part 2/02- Objects/8- Adding or Removing Properties/8- Adding or Removing Properties.js
+++ part 2/02- Objects/8- Adding or Removing Properties/8- Adding or Removing Properties.js
@@ -1,3 +1,6 @@
+/* -----------------------------------------------------------
+ *                  Constructor Function
+ * ----------------------------------------------------------- */
 function Circle(radius) {
   this.radius = radius;
   this.draw = function () {
@@ -7,18 +10,21 @@ function Circle(radius) {
 
 const circle = new Circle(5);
 
-// -----------------------------------------------------------
-// Adding properties using dot notation
-// -----------------------------------------------------------
-circle.location = { x: 1 };
+/* -----------------------------------------------------------
+ *          Adding Properties to an Object
+ * -----------------------------------------------------------
+ * - Dot notation: Use when the property name is known and fixed.
+ * - Bracket notation: Use when the property name is dynamic.
+ */
 
-// -----------------------------------------------------------
-// Adding properties using bracket notation
-// -----------------------------------------------------------
+/* ---- Using Dot Notation ---- */
+circle.location = { x: 1 }; // Preferred when property name is static
+
+/* ---- Using Bracket Notation ---- */
 const propertyName = "location";
-circle[propertyName] = { x: 1 };
+circle[propertyName] = { x: 1 }; // Use when property name is dynamic
 
-// -----------------------------------------------------------
-// Deleting a property
-// -----------------------------------------------------------
+/* -----------------------------------------------------------
+ *                  Deleting a Property
+ * ----------------------------------------------------------- */
 delete circle.location;
