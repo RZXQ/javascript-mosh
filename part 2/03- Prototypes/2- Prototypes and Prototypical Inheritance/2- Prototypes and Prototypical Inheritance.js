8ce1f5ea8b9ec78de9e81237741d268b2f82fdbe
Reacher <macryze@icloud.com>
2025-02-09 10:45:21 +0800

    Update prototype comments and add `getPrototypeOf` usage
    Clarified comments on prototypes and introduced an example using `Object.getPrototypeOf` for better understanding. Highlighted the deprecation of `__proto__` and added a demonstration of prototype comparison. These changes improve code clarity and modernize the example.

--- part 2/03- Prototypes/2- Prototypes and Prototypical Inheritance/2- Prototypes and Prototypical Inheritance.js
+++ part 2/03- Prototypes/2- Prototypes and Prototypical Inheritance/2- Prototypes and Prototypical Inheritance.js
@@ -1,6 +1,13 @@
-// A prototype is just a regular object.
-// Every object has a prototype, except the ObjectBase
+// ===========================================================
+//                      getPrototypeOf(x)
+// ===========================================================
+// Note:
+// 1. x.__proto__ is deprecated
+// 2. A prototype is just a regular object.
+// 3. Every object has a prototype, except the ObjectBase
 
 let x = {};
 let y = {};
+
+// Check if two objects share the same prototype
 console.log(Object.getPrototypeOf(x) === Object.getPrototypeOf(y)); // true
