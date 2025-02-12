9830ad395cf8c547fd9af203a218927a5ca067f8
Reacher <macryze@icloud.com>
2025-02-13 01:11:36 +0800

    Update comments for clarity on constructor functions and methods
    Revised comments to better reflect ES6 class behavior, distinguishing instance-specific methods from prototype methods. Added a type-checking example for improved demonstration of ES6 classes.

--- part 2/05- ES6 Classes/1- ES6 Classes/1- ES6 Classes.js
+++ part 2/05- ES6 Classes/1- ES6 Classes/1- ES6 Classes.js
@@ -1,5 +1,5 @@
 // ===========================================================
-//                      Constructor Function
+//            Constructor Function (Pre-ES6)
 // ===========================================================
 function Circle(radius) {
   this.radius = radius;
@@ -9,21 +9,23 @@ function Circle(radius) {
 }
 
 // ===========================================================
-//                      ES6 Class: Syntax Sugar
+//              ES6 Class (Syntactic Sugar)
 // ===========================================================
 class Circle {
   constructor(radius) {
     this.radius = radius;
-    // A. Method defined here is not in prototype
+
+    // A. Method defined here is created PER INSTANCE (not in prototype)
     this.move = function () {
       console.log("move");
     };
   }
 
-  // B. draw method is defined in prototype (not inside constructor)
+  // B. Method defined here is in PROTOTYPE (shared between instances)
   draw() {
     console.log("draw");
   }
 }
 
-const circle = new Circle();
+const circle = new Circle(1);
+console.log(typeof Circle); // Output: "function"
