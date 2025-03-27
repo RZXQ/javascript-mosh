8080ca5cb0863807e661ec68932dfe8b793b3dcc
Reacher <macryze@icloud.com>
2025-03-27 15:57:22 +0800

    Add function description for `isLandscape`
    Provided a detailed description of the `isLandscape` function, explaining its purpose, parameters, and return values. This improves code readability and aids understanding for future reference.

--- part 1/04- Control Flow/11- Exercise 2- Landscape or Portrait/11- Exercise 2- Landscape or Portrait.js
+++ part 1/04- Control Flow/11- Exercise 2- Landscape or Portrait/11- Exercise 2- Landscape or Portrait.js
@@ -1,3 +1,9 @@
+// Description:
+// The function `isLandscape` determines if an image is in landscape orientation.
+// It takes two parameters, `width` and `height`,
+// and returns `true` if the `width` is greater than the `height`,
+// indicating a landscape orientation. Otherwise, it returns `false`.
+
 console.log(isLandscape(300, 600));
 
 function isLandscape(width, height) {
