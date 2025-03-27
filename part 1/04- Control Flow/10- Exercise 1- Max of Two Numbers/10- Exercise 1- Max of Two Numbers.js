9bc8f6ae0be23441d35abd053b1f35e4e41059f7
Reacher <macryze@icloud.com>
2025-03-27 15:39:51 +0800

    Add description comment to max function implementation
    Added a description explaining the purpose of the `max` function. This clarifies its behavior for future readers and improves code readability. No functional changes were made.

--- part 1/04- Control Flow/10- Exercise 1- Max of Two Numbers/10- Exercise 1- Max of Two Numbers.js
+++ part 1/04- Control Flow/10- Exercise 1- Max of Two Numbers/10- Exercise 1- Max of Two Numbers.js
@@ -1,5 +1,8 @@
-console.log(max(5, 3));
+// Description:
+// write a function that takes two numbers and returns the maximum of the two
 
 function max(a, b) {
   return a > b ? a : b;
 }
+
+console.log(max(5, 3));
