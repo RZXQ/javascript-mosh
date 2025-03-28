d1e3d8e6850232e90a30994a1143c8c2cd31c0b1
Reacher <macryze@icloud.com>
2025-03-28 23:40:15 +0800

    Refactor factory functions to compare syntax styles
    Separated factory functions into two versions: one using full syntax and another employing ES6 shorthand. Added comments to clarify differences in syntax and behavior.

--- part 1/05- Objects/2- Factory Functions/2- Factory Functions.js
+++ part 1/05- Objects/2- Factory Functions/2- Factory Functions.js
@@ -1,14 +1,29 @@
-function createCircle(radius) {
+// Factory function using full syntax to create a circle object
+function createCircle1(radius) {
   return {
-    radius,
-    draw() {
+    radius: radius, // explicitly defining 'radius' property
+    draw: function () {
+      // explicitly defining 'draw' method
       console.log("draw");
     },
   };
 }
 
-const circle1 = createCircle(1);
+// Factory function using shorter ES6 syntax for object literals and methods
+function createCircle2(radius) {
+  return {
+    radius, // shorter syntax (ES6): automatically assigns property from parameter
+    draw() {
+      // concise method definition (ES6 shorthand syntax)
+      console.log("draw");
+    },
+  };
+}
+
+// Creating and logging circle object made with full syntax
+const circle1 = createCircle1(1);
 console.log(circle1);
 
-const circle2 = createCircle(2);
+// Creating and logging circle object made with shorter ES6 syntax
+const circle2 = createCircle2(2);
 console.log(circle2);
