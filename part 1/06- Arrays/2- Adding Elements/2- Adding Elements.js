1ec683e4b54fd241e451591e370c3122c6341e18
Reacher <macryze@icloud.com>
2025-03-30 17:51:32 +0800

    Refactor array method examples to include return values
    Updated comments and examples for push, unshift, and splice to demonstrate their return values. This improves clarity and provides important context for understanding the behavior of these methods.

--- part 1/06- Arrays/2- Adding Elements/2- Adding Elements.js
+++ part 1/06- Arrays/2- Adding Elements/2- Adding Elements.js
@@ -1,15 +1,23 @@
 // ===========================================================
-//                      numbers.push()
+//                      Array.push()
+// returns: The new length  property of the object upon which the method was called.
 // ===========================================================
+
 const numbers = [3, 4];
-numbers.push(5, 6);
+const pushReturn = numbers.push(5, 6); // returns 4
 
 // ===========================================================
-//                      numbers.unshift()
+//                      Array.unshift()
+// returns: The new length  property of the object upon which the method was called.
 // ===========================================================
-numbers.unshift(1, 2);
+
+const unshiftReturn = numbers.unshift(1, 2); // returns 6
 
 // ===========================================================
-//                      numbers.splice()
+//                      Array.splice()
+// returns: An array containing the deleted elements.
+// If only one element is removed, an array of one element is returned.
+// If no elements are removed, an empty array is returned.
 // ===========================================================
-numbers.splice(2, 0, "a", "b");
+
+const spliceReturn = numbers.splice(2, 0, "a", "b"); // returns []
