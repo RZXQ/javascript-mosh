19acbb59928cf7a17e2070b7b35023a2e2e41e85
Reacher <macryze@icloud.com>
2025-04-09 14:42:21 +0800

    Refactor examples of getters and setters in JavaScript.
    Updated the code to include a comparison between object literal and `Object.defineProperty` forms for defining getters and setters. Enhanced clarity and provided a detailed commentary for better understanding.

--- part 1/07- Functions/6- Getters and Setters/6- Getters and Setters.js
+++ part 1/07- Functions/6- Getters and Setters/6- Getters and Setters.js
@@ -1,22 +1,32 @@
-// ===========================================================
-//                      Object Getters & Setters
-// ===========================================================
+/*                             Getter / Setter Definition Forms:
+=======================================================================================================================
+| Aspect                | Object Literal Form                           | Object.defineProperty() Form                 |
+|-----------------------|-----------------------------------------------|----------------------------------------------|
+| Syntax clarity        | Cleaner, concise                              | More verbose, explicit                       |
+| Descriptor control    | Defaults (`enumerable`, `configurable`: true) | Explicit fine-grained control                |
+| Context (`this`)      | Automatically refers to object                | Explicit (`this` may differ with arrow functions)|
+| Property overriding   | Can be easily overridden implicitly           | Overrides explicitly                         |
+====================================================================================================================== */
 
+// A. Object Literal Form
 const person = {
-  firstName: "Mosh",
-  lastName: "Hamedani",
+  firstName: "Ruizhi",
+  lastName: "WANG",
 
-  // Getter
+  set fullName(value) {
+    [this.firstName, this.lastName] = value.split(" ");
+  },
+
   get fullName() {
     return `${this.firstName} ${this.lastName}`;
   },
-
-  // Setter
-  set fullName(value) {
-    this.firstName = value.split(" ")[0];
-    this.lastName = value.split(" ")[1];
-  },
 };
+
+// B. Object.defineProperty() Form
+Object.defineProperty(person, "fullName", {
+  set: (value) => ([person.firstName, person.lastName] = value.split(" ")),
+  get: () => `${person.firstName} ${person.lastName}`,
+});
 
 person.fullName = "Ruizhi WANG";
 console.log(person);
