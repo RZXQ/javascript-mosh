259453cc424b2b638eb93e37df7df5d1f8920636
Reacher <macryze@icloud.com>
2025-05-07 10:56:13 +0800

    simplifies  comments

--- part 2/04- Prototypical Inheritance/5- Method Overriding/5- Method Overriding.js
+++ part 2/04- Prototypical Inheritance/5- Method Overriding/5- Method Overriding.js
@@ -1,46 +1,25 @@
-// ===========================================================
-//                      Parent Class: Shape
-// ===========================================================
-function Shape(color) {
-  this.color = color;
-}
-
-Shape.prototype.duplicate = function () {
-  console.log(this.color); // Demonstrates the use of the 'color' property
-  console.log("duplicate");
-};
-
-// ===========================================================
-//                  Child Class: Circle
-// ===========================================================
-function Circle(color, radius) {
-  Shape.call(this, color); // Call the parent constructor with 'this' and 'color'
-  this.radius = radius;
-}
-
-// ===========================================================
-//                    Utility Function: extend
-// ===========================================================
 function extend(Child, Parent) {
   Child.prototype = Object.create(Parent.prototype);
   Child.prototype.constructor = Child;
 }
 
-// ===========================================================
-//                      Extend Classes
-// ===========================================================
-extend(Circle, Shape);
+function Shape() {}
 
-// ===========================================================
-//                   Method Override: Circle
-// ===========================================================
-Circle.prototype.duplicate = function () {
-  Shape.prototype.duplicate.call(this); // Call parent method with correct 'this' binding
-  console.log("duplicate circle");
+Shape.prototype.duplicate = function () {
+  console.log("Duplicate in Shape");
 };
 
-// ===========================================================
-//                      Usage Example
-// ===========================================================
-const circleInstance = new Circle("red", 5);
-circleInstance.duplicate();
+function Circle() {}
+
+extend(Circle, Shape);
+
+// Override the default behavior in Shape
+Circle.prototype.duplicate = function () {
+  console.log("Duplicate in Circle");
+  // Shape.prototype.duplicate(); // if not using `this` in parent's duplicate method, both works
+  // Shape.prototype.duplicate.call(this); // if using `this` in parent's duplicate method
+};
+
+const c = new Circle(10);
+c.duplicate();
+console.log(c);
